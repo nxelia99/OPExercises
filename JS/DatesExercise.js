11dd5496dd83eeeac936c62cb28caa3573dd471e
Nxelia <55344006+nxelia99@users.noreply.github.com>
2022-12-07 19:54:41 +0100

    Dates Exercise
    Enunciado del ejercicio:
    
    Crea un archivo llamado fechas.js que contenga las siguientes líneas
    
    - La fecha de hoy
    
    - La fecha de tu nacimiento
    
    - Un variable que indique si hoy es más tarde (>) que la fecha de tu nacimiento
    
    - Una variable que contenga el día de tu nacimiento
    
    - Una variable que contenga el mes de tu nacimiento (recuerda que Enero es mes 0)
    
    - Una variable que contenga el año de tu nacimiento (con 4 dígitos)

---
+++ JS/DatesExercise.js
@@ -0,0 +1,13 @@
+const today = new Date("December, 07, 2022")
+console.log(today)
+
+const mybirthdate = new Date("September, 16, 1999")
+console.log(mybirthdate)
+
+console.log(today > mybirthdate)
+
+const birthDay = 16
+const birthMonth = 9
+const birthYear = 1999
+
+console.log(birthDay + '/' + birthMonth + '/' + birthYear)

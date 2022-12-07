8b7515d696757ceed384f783031c3dc9042c2567
Nxelia <55344006+nxelia99@users.noreply.github.com>
2022-12-07 19:37:22 +0100

    Set exercise
    Enunciado del ejercicio:
    
    Crea un archivo llamado conjuntos.js que contenga las siguientes líneas
    
    - Un nuevo Set con los nombres de tu familia
    
    - Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
    
    - Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)

---
+++ JS/setsExercise1.js
@@ -0,0 +1,6 @@
+const family = ['Michael', "Sage", "Skye", "Lisa"]
+const set = new Set(family) //create set from an array
+set.add("Skye")
+console.log(set)
+set.add("Javascript")
+console.log(set)

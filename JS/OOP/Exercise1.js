12c11fc6a52dd06a2d4eed35afde52dc2f87779a
Nxelia <55344006+nxelia99@users.noreply.github.com>
2022-12-14 12:49:14 +0100

    Update Exercise1.js

--- JS/OOP/Exercise1.js
+++ JS/OOP/Exercise1.js
@@ -1,4 +1,17 @@
-//declare the class
+Crea un nuevo fichero JS que contenga las siguientes líneas
+
+- Una clase llamada "Estudiante" que tenga:
+
+- Una variable llamada nombre
+
+- Otra variable lista llamada asignaturas con 3 asignaturas: Javascript, HTML, CSS
+
+- Un método "obtenDatos" que devuelva un objeto con las propiedades nombre y asignaturas
+
+- Crea una nueva instancia de "Estudiante"
+
+- Haz la llamada al método obtenDatos
+
 
 class Student {
     constructor(name, list){

dede9a3efc8b5d687a284ec06626af10b0245321
Nxelia <55344006+nxelia99@users.noreply.github.com>
2022-12-14 12:50:03 +0100

    Update Exercise1.js

--- JS/OOP/Exercise1.js
+++ JS/OOP/Exercise1.js
@@ -1,16 +1,16 @@
-Crea un nuevo fichero JS que contenga las siguientes líneas
+//Crea un nuevo fichero JS que contenga las siguientes líneas
 
-- Una clase llamada "Estudiante" que tenga:
+//- Una clase llamada "Estudiante" que tenga:
 
-- Una variable llamada nombre
+//- Una variable llamada nombre
 
-- Otra variable lista llamada asignaturas con 3 asignaturas: Javascript, HTML, CSS
+//- Otra variable lista llamada asignaturas con 3 asignaturas: Javascript, HTML, CSS
 
-- Un método "obtenDatos" que devuelva un objeto con las propiedades nombre y asignaturas
+//- Un método "obtenDatos" que devuelva un objeto con las propiedades nombre y asignaturas
 
-- Crea una nueva instancia de "Estudiante"
+//- Crea una nueva instancia de "Estudiante"
 
-- Haz la llamada al método obtenDatos
+//- Haz la llamada al método obtenDatos
 
 
 class Student {

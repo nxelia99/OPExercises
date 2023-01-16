054bde58a2564d01b4763ce2c5f6df7b0644145d
Nxelia <55344006+nxelia99@users.noreply.github.com>
2023-01-16 19:05:50 +0100

    Update MotoMain.java

--- Java/ExerciseClasses/MotoMain.java
+++ Java/ExerciseClasses/MotoMain.java
@@ -11,11 +11,12 @@ public class MotoMain {
         moto1.setMarca("KTM");
         moto1.setPeso(119);
         moto1.setKilometraje(175.8);
-
+        
         System.out.println("El color de la moto1 es: " + moto1.getAtributo1());
         System.out.println("La marca de la moto1 es: " + moto1.getAtributo2());
         System.out.println("El peso de la moto1 es: " + moto1.getAtributo3());
         System.out.println("El kilometraje de la moto1 es: " + moto1.getAtributo4());
+        System.out.println(moto1.acelerar(30)); // acelera
 
         //creación moto2
         Moto moto2 = new Moto();

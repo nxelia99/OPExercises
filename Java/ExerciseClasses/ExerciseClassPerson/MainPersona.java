580e0eeb19e1afe6a98c1a2964a06597670dbc79
Nxelia <55344006+nxelia99@users.noreply.github.com>
2023-01-25 21:17:37 +0100

    Update MainPersona.java

--- Java/ExerciseClasses/ExerciseClassPerson/MainPersona.java
+++ Java/ExerciseClasses/ExerciseClassPerson/MainPersona.java
@@ -24,21 +24,21 @@ public class MainPersona {
         // creación de profesor1
         Profesor profesor1 = new Profesor("Alejandro", "Ruiz Delgado", "Hombre", 35, 1.78, "Literatura", 12834);
         System.out.println("\nDatos de profesor1: ");
-        System.out.println("\nNombre de profesor1: " + profesor1.getNombre());
-        System.out.println("Apellidos de profesor1: " + profesor1.getApellidos());
-        System.out.println("Género de profesor1: " + profesor1.getGenero());
-        System.out.println("Edad de profesor1: " + profesor1.getEdad());
-        System.out.println("Altura de profesor1: " + profesor1.getAltura());
+        System.out.println("\nNombre: " + profesor1.getNombre());
+        System.out.println("Apellidos: " + profesor1.getApellidos());
+        System.out.println("Género: " + profesor1.getGenero());
+        System.out.println("Edad: " + profesor1.getEdad());
+        System.out.println("Altura: " + profesor1.getAltura());
         System.out.println("Departamento: " + profesor1.getDepartamento());
         System.out.println("Id de licencia: " + profesor1.getIdlicencia());
 
         Profesor profesor2 = new Profesor("Miriam", "De la Rosa Vélez", "Mujer", 28, 1.67, "Tecnología", 11502);
         System.out.println("\nDatos de profesor2: ");
-        System.out.println("\nNombre de profesor1: " + profesor2.getNombre());
-        System.out.println("Apellidos de profesor1: " + profesor2.getApellidos());
-        System.out.println("Género de profesor1: " + profesor2.getGenero());
-        System.out.println("Edad de profesor1: " + profesor2.getEdad());
-        System.out.println("Altura de profesor1: " + profesor2.getAltura());
+        System.out.println("\nNombre: " + profesor2.getNombre());
+        System.out.println("Apellidos: " + profesor2.getApellidos());
+        System.out.println("Género: " + profesor2.getGenero());
+        System.out.println("Edad: " + profesor2.getEdad());
+        System.out.println("Altura: " + profesor2.getAltura());
         System.out.println("Departamento: " + profesor2.getDepartamento());
         System.out.println("Id de licencia: " + profesor2.getIdlicencia());
     }

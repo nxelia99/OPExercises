9cfab42f5e303e4a8261cdf41c37de8963087540
Nxelia <55344006+nxelia99@users.noreply.github.com>
2023-04-27 18:21:55 +0200

    Update ClasD.java

--- Java/ExerciseClasses/Inheritance/ClasD.java
+++ Java/ExerciseClasses/Inheritance/ClasD.java
@@ -1,3 +1,5 @@
+package com.tareaabrilprogramacion;
+
 public class ClasD extends ClasB implements InterA, InterB{
     private final int atr1_clasD = 3;
     private double atr2_clasD;
@@ -16,7 +18,7 @@ public class ClasD extends ClasB implements InterA, InterB{
         return atr2_clasD;
     }
     public void setAtr2_clasD(double atr2_clasD){
-        this.atr2_clasA = atr2_clasA;
+        this.atr2_clasD = atr2_clasD;
     }
     public String getAtr3_clasD(){
         return atr3_clasD;
@@ -26,15 +28,29 @@ public class ClasD extends ClasB implements InterA, InterB{
     }
 
     @Override
+    public EnumA getEnum1() {
+        return super.getEnum1();
+    }
+
+    @Override
+    public void setEnum1(EnumA enum1) {
+        super.setEnum1(enum1);
+    }
+
+    @Override
     public double metodoAbsClasB(int numero) {
-        return numero * 0.76;
+        return numero * atr1_clasD;
     }
 
     @Override
     public String toString(){
-        return super.toString() + ", ClasD [atr1_clasD=" + atr1_clasD + ", atr2_clasD=" + atr2_clasD + ", atr3_clasD=" + atr3_clasD;
+        return super.toString() + "\nClasD: atr1_clasD: " + atr1_clasD + ", atr2_clasD: " + atr2_clasD + ", atr3_clasD: " + atr3_clasD + " de tipo String en objD";
     }
-    public void print1(String text){
+    public void printA(String text){
         System.out.println("método de la interfaz B en la clase D");
+    }
+
+    public void printB(String textB) {
+        System.out.println("método de la interface B en clasE");
     }
 }

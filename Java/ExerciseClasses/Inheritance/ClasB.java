20b1bd778d47329ac0d1207a99fa718e4dccbb58
Nxelia <55344006+nxelia99@users.noreply.github.com>
2023-05-02 10:53:31 +0200

    Update ClasB.java

--- Java/ExerciseClasses/Inheritance/ClasB.java
+++ Java/ExerciseClasses/Inheritance/ClasB.java
@@ -1,15 +1,15 @@
 // 1. Clase abstracta.
 public abstract class ClasB extends ClasA{
     // 1atributo de tipo enumerado
-    EnumA enum1 = EnumA.VALOR1;
+    EnumA enum1;
 
-    //Constructor con el total de parámetros
-    public ClasB(EnumA enum1, int atr1_clasA, String atr2_clasA) {
-        super(atr1_clasA, atr2_clasA);
+    //Constructor con el total de parametros
+    public ClasB(EnumA enum1, int atr1_clasA, String atr2_clasA, String nombre) {
+        super(atr1_clasA, atr2_clasA, nombre);
         this.enum1 = enum1;
     }
 
-    //Métodos getter y setter para cada atributo.
+    //Metodos getter y setter para cada atributo.
 
     public EnumA getEnum1() {
         return enum1;
@@ -19,18 +19,18 @@ public abstract class ClasB extends ClasA{
         this.enum1 = enum1;
     }
 
-    //1 método abstracto que reciba un parámetro entero y devuelva un número con
-    //decimales resultado de realizar la operación siguiente:
+    //1 metodo abstracto que reciba un parametro entero y devuelva un numero con
+    //decimales resultado de realizar la operacion siguiente:
     //(valor del parametro multiplicado por la constante indicada en la clase que
-    //implementa el método). Ten en cuenta donde se declaran y donde se implementan
-    //los métodos abstractos.
+    //implementa el metodo). Ten en cuenta donde se declaran y donde se implementan
+    //los metodos abstractos.
 
     public abstract double metodoAbsClasB(int numero);
 
 
 
-    //6. Reescritura del método toString para mostrar los datos de esta clase pero también
-    //se debe usar el método toString del padre(super.toString()).
+    //6. Reescritura del metodo toString para mostrar los datos de esta clase pero tambien
+    //se debe usar el metodo toString del padre(super.toString()).
 
     @Override
     public String toString() {

a4604ce6044c16ab29ffeb61c7bd60d4485f42fa
Nxelia <55344006+nxelia99@users.noreply.github.com>
2023-05-02 10:53:56 +0200

    Update ClasC.java

--- Java/ExerciseClasses/Inheritance/ClasC.java
+++ Java/ExerciseClasses/Inheritance/ClasC.java
@@ -6,10 +6,10 @@ public class ClasC extends ClasA{
     protected int atr1_clasC;
     protected boolean atr2_clasC;
 
-    //3. Constructor con el total de parámetros.
+    //3. Constructor con el total de parametros.
 
-    public ClasC(int atr1_clasC, boolean atr2_clasC, int atr1_clasA, String atr2_clasA) {
-        super(atr1_clasA, atr2_clasA);
+    public ClasC(int atr1_clasC, boolean atr2_clasC, int atr1_clasA, String atr2_clasA, String nombre) {
+        super(atr1_clasA, atr2_clasA, nombre);
         this.atr1_clasC = atr1_clasC;
         this.atr2_clasC = atr2_clasC;
     }
@@ -32,12 +32,14 @@ public class ClasC extends ClasA{
     public void setAtr2_clasC(boolean atr2_clasC) {
         this.atr2_clasC = atr2_clasC;
     }
-    //5. Reescritura del método toString para mostrar los datos de esta clase pero también
-    //se debe usar el método toString del padre(super.toString()
+
+    //5. Reescritura del metodo toString para mostrar los datos de esta clase pero tambien
+    //se debe usar el metodo toString del padre(super.toString()
 
     @Override
     public String toString(){
         return super.toString() + "\nClasC: atr1_clasC: " + atr1_clasC + ", atr2_clasC: " + atr2_clasC;
     }
+
 
 }

5a2d6fd6c3a332a918dc60489104a727cb262c84
Nxelia <55344006+nxelia99@users.noreply.github.com>
2023-05-13 19:05:40 +0200

    Create Main.java

---
+++ Java/SQLJava/SQL-Insert-Delete/Main.java
@@ -0,0 +1,19 @@
+
+
+import java.sql.SQLException;
+
+public class Main {
+
+
+     public static void main(String[] args) throws SQLException {
+
+         ModuloDAO si = new ModuloDAO(1, "programacion", 120, "Juan Cruz", 12, 8);
+         si.insertarModulo();
+
+         ModuloDAO no = new ModuloDAO(2, "LDM", 120, "Lola Lele", 9, 7);
+         no.insertarModulo();
+         si.eliminarModulo(1);
+
+
+    }
+}

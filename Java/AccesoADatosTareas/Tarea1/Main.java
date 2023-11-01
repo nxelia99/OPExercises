e71defdb8c53d8fe0b1b9098dae18428bfdd7e82
Nxelia <55344006+nxelia99@users.noreply.github.com>
2023-11-01 20:05:15 +0100

    Create Main.java

---
+++ Java/AccesoADatosTareas/Tarea1/Main.java
@@ -0,0 +1,15 @@
+package com.example.tarea1ac;
+
+
+import java.io.IOException;
+
+
+
+
+    public class Main{
+    public static void main(String[] args) throws IOException {
+        Fichero fichero = new Fichero();
+        fichero.leer("archivo.txt");
+
+    }
+}

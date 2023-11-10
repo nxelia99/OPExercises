b9dd26582c7adab4be28ecc897de17f905020947
Nxelia <55344006+nxelia99@users.noreply.github.com>
2023-11-10 18:54:53 +0100

    Update Main.java

--- Java/AccesoADatosTareas/Tarea1/Main.java
+++ Java/AccesoADatosTareas/Tarea1/Main.java
@@ -4,12 +4,11 @@ package com.example.tarea1ac;
 import java.io.IOException;
 
 
-
-
     public class Main{
     public static void main(String[] args) throws IOException {
         Fichero fichero = new Fichero();
         fichero.leer("archivo.txt");
+        fichero.estadistica("archivo.txt");
 
     }
 }

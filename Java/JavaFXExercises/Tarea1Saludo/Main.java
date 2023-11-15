c16431739ee09bc74ae1c5c5fd2ac102e4616d2e
Nxelia <55344006+nxelia99@users.noreply.github.com>
2023-11-15 19:11:07 +0100

    Update Main.java

--- Java/JavaFXExercises/Tarea1Saludo/Main.java
+++ Java/JavaFXExercises/Tarea1Saludo/Main.java
@@ -7,7 +7,7 @@ public class Main extends Application {
 @Override
  public void start(Stage primaryStage) throws Exception{
 
-Parent root = FXMLLoader.load(getClass().getResource("/Interfaz2.fxml"));
+Parent root = FXMLLoader.load(getClass().getResource("/Interfaz.fxml"));
 
 Scene scene = new Scene(root);
 primaryStage.setScene(scene);

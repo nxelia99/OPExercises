dea7efd05bb60c6bc587f30cda282ca438edb483
Nxelia <55344006+nxelia99@users.noreply.github.com>
2024-01-26 18:56:45 +0100

    Create Main.java

---
+++ Java/arrayExercises/Exercise1/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        Sort.intsortarray();
+        Sort.stringsortarray();
+        }
+    }

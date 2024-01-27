27040f9c5c46ca96f182ef0486f72fb6ec0fe352
Nxelia <55344006+nxelia99@users.noreply.github.com>
2024-01-27 20:42:34 +0100

    Create Main.java

---
+++ Java/arrayExercises/AvgArray/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    Avg.avgarray();
+        }
+    }

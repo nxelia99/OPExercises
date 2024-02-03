f906ccd2de941299e8d6d5893994b88e98e5758c
Nxelia <55344006+nxelia99@users.noreply.github.com>
2024-02-03 19:35:59 +0100

    Create DirectorEmpleado.java

---
+++ Java/SpringCurso/ProyectoUno/DirectorEmpleado.java
@@ -0,0 +1,9 @@
+public class DirectorEmpleado implements Empleados {
+
+	@Override
+	public String getTareas() {
+		// TODO Auto-generated method stub
+		return "Gestionar la plantilla de la empresa";
+	}
+
+}

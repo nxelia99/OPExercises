bee5836e6e94bd0c7e2c1d234fe8cdf140836a84
Nxelia <55344006+nxelia99@users.noreply.github.com>
2024-02-06 12:08:53 +0100

    Update SecretarioEmpleado.java

--- Java/SpringCurso/ProyectoUno/SecretarioEmpleado.java
+++ Java/SpringCurso/ProyectoUno/SecretarioEmpleado.java
@@ -3,6 +3,43 @@ public class SecretarioEmpleado implements Empleados{
 	public String getTareas() {
 		return "Gestionar la agenda de los Jefes";
 	}
+	@Override
+	public String getInforme() {
+		// TODO Auto-generated method stub
+		return "Informe generado por el Secretario: " + informeNuevo.getInforme() ; 
+	}
+	
+	//setter para la inyección
+	private CreacionInformes informeNuevo;
+	public void setInformeNuevo(CreacionInformes informeNuevo) {
+		this.informeNuevo = informeNuevo;
+	}
+	
+	//creamos campos
+	private String email;
+	private String nombreEmpresa;
+	
+	//setters y getters establece valor de los campos desde ApplicatonContext
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNombreEmpresa() {
+		return nombreEmpresa;
+	}
+
+	public void setNombreEmpresa(String nombreEmpresa) {
+		this.nombreEmpresa = nombreEmpresa;
+	}
+	
+	
+	
+
+
 	
 	
 

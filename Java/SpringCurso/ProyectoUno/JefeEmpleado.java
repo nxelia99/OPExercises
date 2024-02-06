88ea4c4dadf32b5d441d7d2238c5c05fd518fd1c
Nxelia <55344006+nxelia99@users.noreply.github.com>
2024-02-06 12:11:32 +0100

    Update JefeEmpleado.java

--- Java/SpringCurso/ProyectoUno/JefeEmpleado.java
+++ Java/SpringCurso/ProyectoUno/JefeEmpleado.java
@@ -1,8 +1,25 @@
 public class JefeEmpleado implements Empleados{
+	
+	//creación del constructor
+	private CreacionInformes informeNuevo;
+	public JefeEmpleado(CreacionInformes informeNuevo) {
+		this.informeNuevo = informeNuevo;
+	}
+	
 	public String getTareas() {
 		return "Gestiono las cuestiones relativas a mis empleados "
 				+ "de sección";	
 	}
+	@Override
+	public String getInforme() {
+		// TODO Auto-generated method stub
+		return "Informe presentado por el jefe con arreglos: " + informeNuevo.getInforme();
+	}
+	public void setInformeNuevo(CreacionInformes informeNuevo) {
+		this.informeNuevo = informeNuevo;
+	}
+	
+	
 	
 	
 }

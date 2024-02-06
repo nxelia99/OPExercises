18bf984f3d1948ebb2907f863d1130b05e76cdae
Nxelia <55344006+nxelia99@users.noreply.github.com>
2024-02-06 12:10:42 +0100

    Update UsoEmpleados.java

--- Java/SpringCurso/ProyectoUno/UsoEmpleados.java
+++ Java/SpringCurso/ProyectoUno/UsoEmpleados.java
@@ -9,11 +9,15 @@ public class UsoEmpleados{
 		ClassPathXmlApplicationContext contexto = new ClassPathXmlApplicationContext ("applicationContext.xml");
 		
 		//pedimos al contexto el Bean con el id y el nombre de la interfaz
+
 		
-		Empleados Juan = contexto.getBean("miEmpleado", Empleados.class);
-		System.out.println(Juan.getTareas());
+		//cerramos contexto, el xml 
 		
-		//cerramos contexto, el xml
+		SecretarioEmpleado María = contexto.getBean("miSecretario", SecretarioEmpleado.class);
+		System.out.println(María.getTareas());
+		System.out.println(María.getInforme());
+		System.out.println("Email: " + María.getEmail());
+		System.out.println("Nombre de empresa: " + María.getNombreEmpresa());
 		
 		contexto.close();
 		

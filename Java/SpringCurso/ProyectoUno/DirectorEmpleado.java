e27d50b844ee20823251c8b0f07ffe7388cdbd4e
Nxelia <55344006+nxelia99@users.noreply.github.com>
2024-02-06 12:11:08 +0100

    Update DirectorEmpleado.java

--- Java/SpringCurso/ProyectoUno/DirectorEmpleado.java
+++ Java/SpringCurso/ProyectoUno/DirectorEmpleado.java
@@ -1,4 +1,11 @@
 public class DirectorEmpleado implements Empleados {
+	
+	//creación del constructor
+	private CreacionInformes informeNuevo;
+	//constructor que inyecta la dependencia
+	public DirectorEmpleado(CreacionInformes informeNuevo) {
+		this.informeNuevo = informeNuevo;
+	}
 
 	@Override
 	public String getTareas() {
@@ -6,4 +13,11 @@ public class DirectorEmpleado implements Empleados {
 		return "Gestionar la plantilla de la empresa";
 	}
 
+	@Override
+	public String getInforme() {
+		// TODO Auto-generated method stub
+		return "Informe creado por el Director: " + informeNuevo.getInforme();
+	}
+
 }
+

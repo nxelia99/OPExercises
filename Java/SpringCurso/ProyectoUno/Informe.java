45df25a130e0be11ca53f0d72703e14f2f76c544
Nxelia <55344006+nxelia99@users.noreply.github.com>
2024-02-06 12:10:17 +0100

    Create Informe.java

---
+++ Java/SpringCurso/ProyectoUno/Informe.java
@@ -0,0 +1,11 @@
+//clase que implementa la interfaz
+
+public class Informe implements CreacionInformes {
+
+	@Override
+	public String getInforme() {
+
+		return "Esta es la presentación del informe";
+	}
+
+}
